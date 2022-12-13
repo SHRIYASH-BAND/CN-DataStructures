23edbe6db86fb4cbbac59c6cdafbca9194a3e433
shriyash <shriyashband@gmail.com>
2022-12-13 13:33:05 +0530

    Initial Commit

---
+++ OOPS1/Fraction.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+
+class Fraction
+{
+private:
+    int numerator;
+    int denominator;
+public:
+    Fraction(/* args */);
+    ~Fraction();
+};
+
+Fraction::Fraction(/* args */)
+{
+}
+
+Fraction::~Fraction()
+{
+}
